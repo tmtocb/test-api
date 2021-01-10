1957616eada414fde83c61b21e08a292a8a6fbd0
tmtocb <kittz.esport@gmail.com>
2021-01-10 23:54:42 +0100

    add Article validations with tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#validations' do
+    it 'should test that factory is valid' do
+      expect(FactoryBot.build :article).to be_valid
+    end
+
+    it 'should validate the presence of the title' do
+      article = FactoryBot.build :article, title: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of the content' do
+      article = FactoryBot.build :article, content: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of slug' do
+      article = FactoryBot.build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate the uniqueness of slug' do
+      article = FactoryBot.create :article
+      invalid_article = FactoryBot.build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+    end
+  end
+  
 end
