e63a2bc0fb844b1d1d1134e59297ee8af70a46c2
tmtocb <kittz.esport@gmail.com>
2021-01-10 23:57:34 +0100

    add factory_bot configuration, refactor tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,32 +4,32 @@ RSpec.describe Article, type: :model do
 
   describe '#validations' do
     it 'should test that factory is valid' do
-      expect(FactoryBot.build :article).to be_valid
+      expect(build :article).to be_valid
     end
 
     it 'should validate the presence of the title' do
-      article = FactoryBot.build :article, title: ''
+      article = build :article, title: ''
       expect(article).not_to be_valid
       expect(article.errors.messages[:title]).to include("can't be blank")
     end
 
     it 'should validate the presence of the content' do
-      article = FactoryBot.build :article, content: ''
+      article = build :article, content: ''
       expect(article).not_to be_valid
       expect(article.errors.messages[:content]).to include("can't be blank")
     end
 
     it 'should validate the presence of slug' do
-      article = FactoryBot.build :article, slug: ''
+      article = build :article, slug: ''
       expect(article).not_to be_valid
       expect(article.errors.messages[:slug]).to include("can't be blank")
     end
 
     it 'should validate the uniqueness of slug' do
-      article = FactoryBot.create :article
-      invalid_article = FactoryBot.build :article, slug: article.slug
+      article = create :article
+      invalid_article = build :article, slug: article.slug
       expect(invalid_article).not_to be_valid
     end
   end
-  
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include FactoryBot::Syntax::Methods
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -61,4 +61,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
 end
