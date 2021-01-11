6cbf096602431f7d1187d9d7a40d67eee136223d
tmtocb <kittz.esport@gmail.com>
2021-01-11 03:19:47 +0100

    add kaminari for pagination, paginate Article data

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    articles = Article.recent
+    articles = Article.recent.page(params[:page]).per(params[:per_page])
     json_articles = ArticleSerializer.new(articles)
     render json: json_articles
   end
